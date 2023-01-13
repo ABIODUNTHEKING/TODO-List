cacb26aa386d26f51cb7e8871fab2d2736f279bc
Emmanuel Abiodun Akinjogunla <105178231+ABIODUNTHEKING@users.noreply.github.com>
2023-01-13 05:34:18 +0100

    DONE

---
+++ script.js
@@ -0,0 +1,37 @@
+let form = document.getElementById("form")
+form.addEventListener("submit", saveTask)
+
+function saveTask(e){
+    e.preventdefault;
+    console.log("I was here")
+    let task = {
+        newTask : document.getElementById("newTask").value
+    }
+    if(localStorage.getItem("task") == null){
+        let setOfTask = []
+        setOfTask.push(task)
+        localStorage.setItem("task", JSON.stringify(setOfTask))
+    }
+    else{
+        let savedTask = JSON.parse(localStorage.getItem("task"))
+        let setOfTask = savedTask.push(task)
+        localStorage.setItem("task", JSON.stringify(setOfTask))
+    }
+    form.reset()
+    taskStructure()
+}
+
+function taskStructure(){
+    
+    let listOfTask = document.getElementById('list')
+    let savedTask = JSON.parse(localStorage.getItem("task"))
+
+    for(let i = 0; i < savedTask.length; i++){
+        let newTask = savedTask[i].task
+
+        listOfTask.innerHTML += '<ul>' +
+                                '<li>' + newTask + '<button class="trash"><i class="fa-duotone fa-trash"></i></button></li>'       
+                                "</ul>"
+    }
+}
+
