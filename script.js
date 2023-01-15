89144915918026b26276b1f3533e19b6ddf44307
Emmanuel Abiodun Akinjogunla <105178231+ABIODUNTHEKING@users.noreply.github.com>
2023-01-15 14:43:33 +0100

    DONE

--- script.js
+++ script.js
@@ -1,37 +1,59 @@
-let form = document.getElementById("form")
-form.addEventListener("submit", saveTask)
-// This is the code that is expected to save the code to the system
-function saveTask(e){
-    e.preventdefault;
-    console.log("I was here")
-    let task = {
-        newTask : document.getElementById("newTask").value
-    }
-    if(localStorage.getItem("task") == null){
-        let setOfTask = []
-        setOfTask.push(task)
-        localStorage.setItem("task", JSON.stringify(setOfTask))
-    }
-    else{
-        let savedTask = JSON.parse(localStorage.getItem("task"))
-        let setOfTask = savedTask.push(task)
-        localStorage.setItem("task", JSON.stringify(setOfTask))
-    }
-    form.reset()
-    taskStructure()
-}
+//VARIABLES
+let inputTask = document.getElementById("task-input");
+let addButton = document.getElementById("addButton");
+let todoListContainer = document.getElementById("todo-div");
+let form = document.getElementById('form')
+let userRequest = document.getElementById('userInput')
 
-function taskStructure(){
+//EVENTLISTENER
+addButton.addEventListener('click', newTask)
+
+//FUNCTIONS
+function newTask(e){
+    e.preventDefault()
+
+    //Creating of the new HTML elements for the todo list container
+
+    //UL
+    let lisetdItem = document.createElement('ul')
+    lisetdItem.classList.add('lisetdItem')
+    //LI
+    let task = document.createElement('li')
+    task.classList.add('task')
+    //SPAN
+    let buttonCollection = document.createElement('span')
+    //DONE BUTTON
+    let doneButton = document.createElement('button')
+    doneButton.classList.add('done-button')
+    doneButton.innerHTML = '<i class="fa-solid fa-square-check"></i>'
+    //TRASH BUTTON
+    let trashButton = document.createElement('button')
+    trashButton.classList.add('trash-button')
+    trashButton.innerHTML = '<i class="fa-solid fa-square-check"></i>'
+
+
+    //Structure the elements
+    todoListContainer.append(lisetdItem)
+    lisetdItem.append(task)
+    task.innerText = inputTask.value
+    task.append(buttonCollection)
+    buttonCollection.append(doneButton)
+    buttonCollection.append(trashButton)
+
+
+if(inputTask.value == ""){
     
-    let listOfTask = document.getElementById('list')
-    let savedTask = JSON.parse(localStorage.getItem("task"))
-
-    for(let i = 0; i < savedTask.length; i++){
-        let newTask = savedTask[i].task
-
-        listOfTask.innerHTML += '<ul>' +
-                                '<li>' + newTask + '<button class="trash"><i class="fa-duotone fa-trash"></i></button></li>'       
-                                "</ul>"
-    }
+    lisetdItem.remove() 
+    alert("Put in a task you want to perform")
 }
+
+
+form.reset()
+}
+
+
+function deleteTask(){
+    
+}
+
 
